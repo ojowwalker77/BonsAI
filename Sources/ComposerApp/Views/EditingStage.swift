69ad2d46ff3a9a2c3b8278772ff9d422a3f49743
ojowwalker77 <jonatasswwalker@gmail.com>
2026-08-11 14:02:03 -0300

    Preserve unchanged shape label geometry

--- Sources/ComposerApp/Views/EditingStage.swift
+++ Sources/ComposerApp/Views/EditingStage.swift
@@ -471,7 +471,9 @@ struct EditingStage: View {
   }
 
   private func commitLabel() {
-    board.setText(card.id, labelDraft)
+    if labelDraft != interaction.text {
+      board.setText(card.id, labelDraft)
+    }
     board.endEditing(card.id)
   }
 
